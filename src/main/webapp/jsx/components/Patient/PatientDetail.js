fc3082a5518e6474a8171c0955341e1370b03d42
Mathew Adegbite <mathewadegbite@gmail.com>
2022-09-28 17:24:56 +0100

    updating prep UI

--- src/main/webapp/jsx/components/Patient/PatientDetail.js
+++ src/main/webapp/jsx/components/Patient/PatientDetail.js
@@ -10,43 +10,13 @@ import { useHistory } from "react-router-dom";
 import SubMenu from './SubMenu';
 import RecentHistory from './../History/RecentHistory';
 import ClinicVisit from '../Consultation/Index'
-import Pharmacy from './../Pharmacy/Index';
-import Laboratory from './../Laboratory/index';
-import AncPnc from './../PmtctServices/AncPnc';
-import AncEnrollement from './../PmtctServices/AncEnrollement';
-import LabourDelivery from './../PmtctServices/LabourDelivery';
 import PrEPCommencementForm from './../PrepServices/PrEPCommencementForm';
 import PrEPDiscontinuationsInterruptions from './../PrepServices/PrEPDiscontinuationsInterruptions';
 import PrEPEligibiltyScreeningForm from './../PrepServices/PrEPEligibiltyScreeningForm';
 import PrEPVisit from './../PrepServices/PrEPVisit';
 import PrEPRegistrationForm from './../PrepServices/PrEPRegistrationForm';
-import EnhancedAdherenceCounseling from '../EnhancedAdherenceCounseling/Index';
-import CervicalCancer from './../CervicalCancer/Index';
-import ClientStatusUpdate from './../ClientStatusUpdate/ClientStatusUpdate'
-//import AdultClinicEvaluationFrom from '../InitailClinicEvaluation/index__'
-import AdultClinicEvaluationForm from '../InitailClinicEvaluation/Adult/Index'
-import ViewAdultClinicEvaluationForm from '../InitailClinicEvaluation/ViewAdultHistory/Index'
-import ChildClinicEvaluationForm from '../InitailClinicEvaluation/ChildClinicEvaluationForm'
-import MentalHealthScreening from '../MentalHealthScreening/index'
-import LabHistory from './../Laboratory/LabHistory'
-import PatientHistory from './../History/PatientHistory'
-import ArtCommencement from './../ArtCommencement/ArtCommencement'
-import ArtCommencementPage from './../ArtCommencement/ArtCommencementPage'
-//History of patient
-import ViewMentalHealthScreening from './../MentalHealthScreening/ViewMhs'
-import ViewAdultClinicEvaluationFrom from './../InitailClinicEvaluation/ViewAdultClinicEvaluationFrom'
-import ViewArtCommencement from './../ArtCommencement/ViewArtCommencement'
-import FirstEac from './../EnhancedAdherenceCounseling/ViewEAC/FirstEac'
-import SecondEac from './../EnhancedAdherenceCounseling/ViewEAC/SecondEac'
-import ThirdEac from './../EnhancedAdherenceCounseling/ViewEAC/ThirdEac'
-import ViewLaboratory from './../Laboratory/ViewLaboratory'
-import PharmacyRefillUpdate from './../Pharmacy/PharmacyRefillUpdate'
 import Biometrics from './Biometric'
-import TrackingForm from './../TrackingForm/Index'
-import FirstEacPage from './../EnhancedAdherenceCounseling/FirstEAC'
-import EACOUTCOME from '../EnhancedAdherenceCounseling/EacOutCome'
-import EACSESSION from './../EnhancedAdherenceCounseling/SessionList'
-import NEWEACSESSION from './../EnhancedAdherenceCounseling/NewSessions'
+
 
 const styles = theme => ({
   root: {
@@ -95,7 +65,7 @@ function PatientCard(props) {
     <div className={classes.root}>
       <div className="row page-titles mx-0" style={{marginTop:"0px", marginBottom:"-10px"}}>
 			<ol className="breadcrumb">
-				<li className="breadcrumb-item active"><h4> <Link to={"/"} >HIV /</Link> Patient Dashboard</h4></li>
+				<li className="breadcrumb-item active"><h4> <Link to={"/"} >PrEp /</Link> Patient Dashboard</h4></li>
 			</ol>
 		  </div>
       <Card >
@@ -107,40 +77,14 @@ function PatientCard(props) {
           {activeContent.route==='biometrics' &&(<Biometrics patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
           {activeContent.route==='consultation' &&( <ClinicVisit patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
           {/* {activeContent==='child-consultation' &&( <ChildConsultation patientObj={patientObj} setActiveContent={setActiveContent}/>)} */}
-          {activeContent.route==='pharmacy' &&( <Pharmacy patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='laboratory' &&( <Laboratory patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='anc-pnc' &&( <AncPnc patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='anc-enrollment' &&( <AncEnrollement patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='labour-delivery' &&( <LabourDelivery patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
           {activeContent.route==='prep-commencement' &&( <PrEPCommencementForm patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
           {activeContent.route==='prep-interruptions' &&( <PrEPDiscontinuationsInterruptions patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
           {activeContent.route==='prep-screening' &&( <PrEPEligibiltyScreeningForm patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
           {activeContent.route==='prep-visit' &&( <PrEPVisit />)}
           {activeContent.route==='prep-registration' &&( <PrEPRegistrationForm patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)} 
-          {activeContent.route==='counseling' &&( <EnhancedAdherenceCounseling patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='cervical-cancer' &&( <CervicalCancer patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='status-update' &&( <ClientStatusUpdate patientObj={patientObj} setActiveContent={setActiveContent}/>)}
-          {activeContent.route==='adult-evaluation' &&( <AdultClinicEvaluationForm patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='child-evaluation' &&( <ChildClinicEvaluationForm patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='mhs' &&( <MentalHealthScreening patientObj={patientObj} setActiveContent={setActiveContent}/>)}
-          {activeContent.route==='lab-history' &&( <LabHistory patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent} />)}
-          {activeContent.route==='patient-history' &&( <PatientHistory patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='art-commencement' &&( <ArtCommencement patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='art-commencementPage' &&( <ArtCommencementPage patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
+                    
           {/* History Pages */}
-          {activeContent.route==='mental-health-view' &&( <ViewMentalHealthScreening patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='adult-clinic-eveluation-view' &&( <ViewAdultClinicEvaluationForm patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='art-commencement-view' &&( <ViewArtCommencement patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='first-eac-history' &&( <FirstEac patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='second-eac-history' &&( <SecondEac patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='completed-eac-history' &&( <ThirdEac patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='lab-view' &&( <ViewLaboratory patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='pharmacy-update' &&( <PharmacyRefillUpdate patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='tracking-form' &&( <TrackingForm patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='first-eac' &&( <FirstEacPage patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='eac-outcome' &&( <EACOUTCOME patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='eac-session' &&( <EACSESSION patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
-          {activeContent.route==='new-eac-session' &&( <NEWEACSESSION patientObj={patientObj} setActiveContent={setActiveContent} activeContent={activeContent}/>)}
+         
          </CardContent>
       </Card>
     </div>
--- src/main/webapp/jsx/components/Patient/SubMenu.js
+++ src/main/webapp/jsx/components/Patient/SubMenu.js
@@ -85,122 +85,17 @@ function SubMenu(props) {
     const onClickHome = (row) =>{        
         props.setActiveContent({...props.activeContent, route:'recent-history'})
     }
-    const loadAncPnc =(row)=> {
-        props.setActiveContent({...props.activeContent, route:'anc-pnc'})
-    }
-    const loadAncAncEnrollment =(row)=> {
-        props.setActiveContent({...props.activeContent, route:'anc-enrollment'})
-    }
-    const loadTrackingForm =(row)=> {
-        props.setActiveContent({...props.activeContent, route:'tracking-form'})
-    }
-    const loadLabourDelivery =(row)=> {
-        props.setActiveContent({...props.activeContent, route:'labour-delivery'})
-    }
-    const loadMentalHealth = ()=>{
-        props.setActiveContent({...props.activeContent, route:'mhs'})
-    }
-    const loadAdultEvaluation =(row)=> {
-        props.setActiveContent({...props.activeContent, route:'adult-evaluation'})
 
-    }
-    const loadPatientHistory = ()=>{
-        props.setActiveContent({...props.activeContent, route:'patient-history'})
-    }
-    const loadArtCommencement = ()=>{
-        props.setActiveContent({...props.activeContent, route:'art-commencementPage'})
-    }
 
     return (
          <div>
-         {/*!props.art && patientObj.commenced!==true && patientObj.enrollment.targetGroupId===473) || (!props.art && (patientObj.commenced!==true || patientObj.commenced===true)  && patientObj.mentalHealth!==true) */}
-            {patientObj.commenced!==true || patientObj.clinicalEvaluation!==true || (patientObj.enrollment.targetGroupId!==473 ? patientObj.mentalHealth!==true : false )?
-                (
-                <Menu size="mini" color={"grey"} inverted >
-                    <Menu.Item onClick={() => onClickHome()} disabled> Home</Menu.Item>
-                    {!patientObj.clinicalEvaluation && (<Menu.Item onClick={() => loadAdultEvaluation()} > Initial Evaluation</Menu.Item>)}
-                    {!patientObj.commenced && (<Menu.Item onClick={() => loadArtCommencement()} >Art Commencement</Menu.Item>)}
-                    <Menu.Item onClick={() => onClickConsultation()} disabled> Clinic Visit</Menu.Item>
-                    <Menu.Item onClick={() => loadLaboratoryModal()} disabled> Laboratory</Menu.Item>
-                    <Menu.Item onClick={() => loadPharmacyModal()} disabled> Pharmacy</Menu.Item>
-                    <Menu.Item onClick={() => loadEAC(patientObj)} disabled> EAC</Menu.Item>
-                    { patientObj.enrollment.targetGroupId!==473 && patientObj.mentalHealth===false && (<Menu.Item onClick={() => loadMentalHealth(patientObj)} >Mental Health Screening</Menu.Item>)}
-                    <Menu.Item onClick={() => loadStatusUpdate(patientObj)} disabled>Client Status Update</Menu.Item>                    
-                    <Menu.Item onClick={() => loadPatientHistory(patientObj)} >History</Menu.Item>
-                    <Dropdown text="PrEP" labeled simple className='icon link item' disabled>
-                            <Dropdown.Menu style={{backgroundColor:"#000", color:"#fff", fontColor:"#fff"}}>
-                                <Dropdown.Item onClick={() => loadPrEPRegistrationForm(patientObj)} disabled> <span  style={{color:"#fff",}}>PrEP Registration</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadPrEPVisitForm(patientObj)} disabled><span  style={{color:"#fff",}}>PrEP Visit</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadPrEPEligibiltyScreeningForm(patientObj)} disabled><span  style={{color:"#fff",}}>PrEP Eligibilty Screening Form</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadPrEPDiscontinuationsInterruptions(patientObj)} disabled><span  style={{color:"#fff",}}>PrEP Discontinuations & Interruptions</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadPrEPCommencementForm(patientObj)} disabled><span  style={{color:"#fff",}}>PrEP Commencement</span></Dropdown.Item>
-                            </Dropdown.Menu>
-                    </Dropdown>
-                            
-                   {props.patientObj.sex==='Female' ? 
-                        (
-                            <>
-                            <Dropdown text="PMTCT"   labeled simple    className='icon link item' disabled>
-                            <Dropdown.Menu style={{backgroundColor:"#000"}}>
-                                <Dropdown.Item onClick={() => loadAncPnc(patientObj)} disabled><span  style={{color:"#fff",}}> ANC and DNC</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadAncAncEnrollment(patientObj)} disabled><span  style={{color:"#fff",}}>ANC Enrolment</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => onClickConsultation(patientObj)} disabled><span  style={{color:"#fff",}}>Child Clinic Visit Follow Up</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadLabourDelivery(patientObj)} disabled><span  style={{color:"#fff",}}>Labour and Delivery</span></Dropdown.Item>                        
-                            </Dropdown.Menu>
-                            </Dropdown>
-                            <Menu.Item onClick={() => loadCervicalCancer(patientObj)} disabled>Cervical Cancer</Menu.Item>
-                            </>
-                        )
-                        :""
-                    } 
-                    
-                    
-                </Menu>
-               )
-               :
-               (
-                <Menu size="mini" color={"black"} inverted>                    
-                    <Menu.Item onClick={() => onClickHome()} disabled={patientCurrentStatus} > Home</Menu.Item>
-                    {/* {patientObj.clinicalEvaluation===true && (<Menu.Item onClick={() => loadAdultEvaluation()} disabled={patientCurrentStatus} >Initial Evaluation</Menu.Item>)} */}
-                    <Menu.Item onClick={() => onClickConsultation()} disabled={patientCurrentStatus}> Clinic Visit</Menu.Item>
-                    <Menu.Item onClick={() => loadLaboratoryModal()} disabled={patientCurrentStatus}> Laboratory</Menu.Item>
-                    <Menu.Item onClick={() => loadPharmacyModal()} disabled={patientCurrentStatus}> Pharmacy</Menu.Item>
-                    <Menu.Item onClick={() => loadEAC(patientObj)} disabled={patientCurrentStatus}> EAC</Menu.Item>
-                    <Menu.Item onClick={() => loadStatusUpdate(patientObj)} >Client Status Update</Menu.Item>
-                    {/* {patientObj.currentStatus && patientObj.currentStatus==='IIT' && (<Menu.Item onClick={() => loadTrackingForm(patientObj)} >Tracking Form</Menu.Item>)} */}
-                    {!patientObj.mentalHealth  && (patientObj.enrollment.targetGroupId!==473) && (<Menu.Item onClick={() => loadMentalHealth(patientObj)} >Mental Health Screening</Menu.Item>)}
-                    <Dropdown text="PrEP" labeled simple className='icon link item'>
-                            <Dropdown.Menu style={{backgroundColor:"#000", color:"#fff", fontColor:"#fff"}}>
-                                <Dropdown.Item onClick={() => loadPrEPRegistrationForm(patientObj)}> <span  style={{color:"#fff",}}>PrEP Registration</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadPrEPVisitForm(patientObj)}><span  style={{color:"#fff",}}>PrEP Visit</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadPrEPEligibiltyScreeningForm(patientObj)}><span  style={{color:"#fff",}}>PrEP Eligibilty Screening Form</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadPrEPDiscontinuationsInterruptions(patientObj)}><span  style={{color:"#fff",}}>PrEP Discontinuations & Interruptions</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadPrEPCommencementForm(patientObj)}><span  style={{color:"#fff",}}>PrEP Commencement</span></Dropdown.Item>
-                            </Dropdown.Menu>
-                    </Dropdown>                            
-                    {props.patientObj.sex==='Female' ? 
-                        (
-                            <>
-                            <Dropdown text="PMTCT"   labeled simple    className='icon link item'>
-                            <Dropdown.Menu style={{backgroundColor:"#000"}}>
-                                <Dropdown.Item onClick={() => loadAncPnc(patientObj)}><span  style={{color:"#fff",}}> ANC and DNC</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadAncAncEnrollment(patientObj)}><span  style={{color:"#fff",}}>ANC Enrolment</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => onClickConsultation(patientObj)}><span  style={{color:"#fff",}}>Child Clinic Visit Follow Up</span></Dropdown.Item>
-                                <Dropdown.Item onClick={() => loadLabourDelivery(patientObj)}><span  style={{color:"#fff",}}>Labour and Delivery</span></Dropdown.Item>                        
-                            </Dropdown.Menu>
-                            </Dropdown>
-                            <Menu.Item onClick={() => loadCervicalCancer(patientObj)}>Cervical Cancer</Menu.Item>
-                            </>
-                        )
-                        :""
-                    } 
-                     {patientObj.currentStatus && patientObj.currentStatus==='IIT' &&  (<Menu.Item onClick={() => loadTrackingForm(patientObj)} >Tracking Form</Menu.Item>)}
-                    <Menu.Item onClick={() => loadPatientHistory(patientObj)} >History</Menu.Item>
-                    
-                </Menu>
-               )
-           }
-                       
+            <Menu size="mini" color={"black"} inverted >
+                <Menu.Item onClick={() => onClickHome()} > Home</Menu.Item>                  
+                <Menu.Item onClick={() => onClickConsultation()} > PrEP Visit</Menu.Item>
+                <Menu.Item onClick={() => loadPrEPEligibiltyScreeningForm()} >PrEP Eligibilty Screening Form</Menu.Item>
+                <Menu.Item onClick={() => loadPrEPDiscontinuationsInterruptions()} >PrEP Discontinuations & Interruptions</Menu.Item>
+                <Menu.Item onClick={() => loadPrEPCommencementForm()} >PrEP Commencement</Menu.Item>                   
+            </Menu>             
         </div>
     );
 }
