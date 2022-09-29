18c2733caf641c45c0e13de6ad3cc6ab0391912c
Mathew Adegbite <mathewadegbite@gmail.com>
2022-09-29 14:22:56 +0100

    removing hiv

--- src/main/java/org/lamisplus/modules/hiv/domain/HIVDomain.java
+++
@@ -1,4 +0,0 @@
-package org.lamisplus.modules.hiv.domain;
-
-public interface HIVDomain {
-}
--- src/main/java/org/lamisplus/modules/hiv/repositories/EacOutComeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.lamisplus.modules.hiv.repositories;
-
-import org.lamisplus.modules.hiv.domain.entity.EacOutCome;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EacOutComeRepository extends JpaRepository<EacOutCome, Long> {
-}
