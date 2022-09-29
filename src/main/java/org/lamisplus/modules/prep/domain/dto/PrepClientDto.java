2a4715c91559c86ce05a6ddbdd0e25a6f5a1acb9
Emeka <ilozuechukwuemeka@yahoo.com>
2022-09-29 22:30:15 +0100

    feat: added repository, controller, services, schema and util

---
+++ src/main/java/org/lamisplus/modules/prep/domain/dto/PrepClientDto.java
@@ -0,0 +1,34 @@
+package org.lamisplus.modules.prep.domain.dto;
+
+import lombok.*;
+import org.lamisplus.modules.patient.domain.dto.PersonResponseDto;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class PrepClientDto implements Serializable {
+    @NotNull(message = "id is mandatory")
+    private Long id;
+    @NotNull(message = "uniqueClientId is mandatory")
+    private String uniqueClientId;
+    @NotNull(message = "dateEnrolled is mandatory")
+    private LocalDate dateEnrolled;
+    @NotNull(message = "populationType is mandatory")
+    private String populationType;
+    @NotNull(message = "partnerType is mandatory")
+    private String partnerType;
+    @NotNull(message = "hivTestingPoint is mandatory")
+    private String hivTestingPoint;
+    @NotNull(message = "dateOfLastHivNegativeTest is mandatory")
+    private LocalDate dateOfLastHivNegativeTest;
+    private LocalDate dateReferredForPrep;
+    @NotNull(message = "personResponseDto is mandatory")
+    private PersonResponseDto personResponseDto;
+    private Object extra;
+}
---
+++ src/main/java/org/lamisplus/modules/prep/repository/PrepClientRepository.java
@@ -0,0 +1,19 @@
+package org.lamisplus.modules.prep.repository;
+
+import org.lamisplus.modules.patient.domain.entity.Person;
+import org.lamisplus.modules.prep.domain.entity.PrepClient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PrepClientRepository extends JpaRepository<PrepClient, Long>, JpaSpecificationExecutor<PrepClient> {
+    List<PrepClient> findAllByPersonOrderByIdDesc(Person person);
+
+    List<PrepClient> findAllByUniqueClientId(String code);
+
+    List<PrepClient> findAllByPerson(Person person);
+
+    Optional<PrepClient> findByIdAndArchivedAndFacilityId(Long id, int archived, Long currentUserOrganization);
+}
