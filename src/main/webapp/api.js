c0d08212286216db62cd570717cc9b9ab6c572f8
Mathew Adegbite <mathewadegbite@gmail.com>
2022-12-10 20:18:14 +0100

    still fixing visit with Emeka

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-// export const url =  'http://localhost:8282/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjcwNTM3NDEzfQ.mJH-EnH0E81PzniqiziG5ted299rDXUEh1-Yi9qLH0WZAAlT9yyptbkK1oE3ZJ1d4-OS3H4IGJsvDNCdXWowXQ';
+// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+// export const url = '/api/v1/'
+export const url =  'http://localhost:8282/api/v1/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjcwNzE2NzUzfQ.z4PCZhMXegJO2-hiKA2amXI3mMHqb4ChC3Biq9KlLSvtxCKfhPLxNbgB2ye9zglkeDnqFG66uJRuGl_N5ktkOg';
