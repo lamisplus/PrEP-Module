caa787f50aeac9754b4ce177be856b8a4d5fc10a
Mathew77 <mathewadegbite@gmail.com>
2023-01-25 13:49:58 +0100

    prep status

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 // export const url =  'http://localhost:8282/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc0NjE3NDE5fQ.nsHdfW5j6IZ8epTpkCD5a2YLe2RPoiBMg0KZVkhQ6wh18i0ScW5fhHxQsn3Ebq-JAg2y6jrDyKq_29VJKPlTfg';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc0Njc3Nzk0fQ.MnwYIlPfIYkFm5C6khUci5UuTS7KsKyI29Ov76-pExdWHhV7RONRS4mocnVA8kvdqbjJjaFX982IxuCaqC9fgQ';
