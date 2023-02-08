3012992e2fa3326bf8b47d9a4912cdedc76069ef
Mathew77 <mathewadegbite@gmail.com>
2023-02-08 10:14:32 +0100

    verifying the feedback

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
-// export const url =  'http://localhost:8282/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1NzY0MjM3fQ.W76bFRdSXxS8DIC_72LVaW4UoulECl4G3ClI7IgE_fMSpRmP6bGdO9LOdnerm6jABkEVfUJzA-1xuCX0Y8J64w'
+// export const url =  'http://localhost:8383/api/v1/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1ODQ5ODc4fQ.o5tz7EwpGdwVSoL7a1PKg5aa5S9LVfGi3WS6dbDfkY_0bVpB_lp4AmWyx46N9NhTO1Va_UxZZNlkrcnaD-t1RA'
