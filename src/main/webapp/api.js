a051a310ceabd2e7dfb764a90992ac40260925d6
Mathew77 <mathewadegbite@gmail.com>
2023-02-09 09:22:23 +0100

    fix prep date given and gender

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
-// export const url =  'http://localhost:8383/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1ODQ5ODc4fQ.o5tz7EwpGdwVSoL7a1PKg5aa5S9LVfGi3WS6dbDfkY_0bVpB_lp4AmWyx46N9NhTO1Va_UxZZNlkrcnaD-t1RA'
+//export const url =  'http://localhost:8383/api/v1/';
+//export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1OTU1NTE2fQ.fYs9lQhhn3Hz6qlbWH4238Aa2eFmeZlbm7vXWhPNPwJrDdCnKABvjOGAapbikhBj1wt_IG985LFsf4RU9FacNg'
