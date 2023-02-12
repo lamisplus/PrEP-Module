85bbcf51715eed8b01a35c2fcd093690f61dc862
Mathew77 <mathewadegbite@gmail.com>
2023-02-12 13:16:56 +0100

    modifying patient dashboard and submenu.js

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-//export const url =  'http://localhost:8383/api/v1/';
-//export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc1OTU1NTE2fQ.fYs9lQhhn3Hz6qlbWH4238Aa2eFmeZlbm7vXWhPNPwJrDdCnKABvjOGAapbikhBj1wt_IG985LFsf4RU9FacNg'
+// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+// export const url = '/api/v1/'
+export const url =  'http://localhost:8383/api/v1/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc2MjI5NTQ5fQ.UqGBfn34eawDICXn1WsTLsH0bvb-MEQ8swwPFXBNXEQFMw6c61LkVKpZO7nrUIJGC4DDKefwkzcyoNXRRLLwjw'
--- src/main/webapp/jsx/components/Patient/SubMenu.js
+++ src/main/webapp/jsx/components/Patient/SubMenu.js
@@ -49,19 +49,20 @@ function SubMenu(props) {
                 </>)
                 :
                 (<>
+                {/* check if the patient has done prep commencement */}
                     {patientObj.prepCount==='0' || patientObj.commencementCount===null ? 
                     (<>
-                        {patientObj.prepCount==='0'  && (<Menu.Item onClick={() => loadPrEPRegistrationForm()} >PrEP Enrollment</Menu.Item>)}
-                        {(patientObj.commencementCount===null || patientObj.commencementCount<=0) && (<Menu.Item onClick={() => loadPrEPCommencementForm()} >PrEP Commencement</Menu.Item>)}
+                        {patientObj.prepCount==='0' && patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPRegistrationForm()} >PrEP Enrollment</Menu.Item>)}
+                        {(patientObj.commencementCount===null || patientObj.commencementCount<=0) && patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPCommencementForm()} >PrEP Commencement</Menu.Item>)}
                         
                     </>) 
                     : (<>
                        
                         <Menu.Item onClick={() => loadPrEPEligibiltyScreeningForm()} > PrEP Eligibility Screening </Menu.Item>
-                        {patientObj.prepCount===null || patientObj.prepCount<0 && (<Menu.Item onClick={() => loadPrEPRegistrationForm()} >PrEP Enrollment</Menu.Item>)}
-                        {patientObj.commencementCount===null || patientObj.commencementCount<=0 && (<Menu.Item onClick={() => loadPrEPCommencementForm()} >PrEP Commencement</Menu.Item>)}
-                        <Menu.Item onClick={() => onClickConsultation()} > PrEP Visit</Menu.Item>
-                        <Menu.Item onClick={() => loadPrEPDiscontinuationsInterruptions()} >PrEP Discontinuations & Interruptions</Menu.Item>
+                        {(patientObj.prepCount===null || patientObj.prepCount<0) && patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPRegistrationForm()} >PrEP Enrollment</Menu.Item>)}
+                        {(patientObj.commencementCount===null || patientObj.commencementCount<=0) && patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPCommencementForm()} >PrEP Commencement</Menu.Item>)}
+                        {patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => onClickConsultation()} > PrEP Visit</Menu.Item>)}
+                        {patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPDiscontinuationsInterruptions()} >PrEP Discontinuations & Interruptions</Menu.Item>)}
                     </>)
                     }
                     
@@ -71,14 +72,14 @@ function SubMenu(props) {
                 :
 
                 (<>
-                {/* This menu will show only if the patient is migrated */}
+                {/* This menu will show only if the patient is migrated  and check if the patient last HIV test result is not positive*/}
                 <Menu.Item onClick={() => onClickHome()} >Home</Menu.Item>
                 <Menu.Item onClick={() => loadPrEPEligibiltyScreeningForm()} >PrEP Eligibility Screening</Menu.Item>
-                <Menu.Item onClick={() => loadPrEPRegistrationForm()} >PrEP Enrollment</Menu.Item>
-                <Menu.Item onClick={() => loadPrEPCommencementForm()} >PrEP Commencement</Menu.Item>
-                <Menu.Item onClick={() => loadPrEPEligibiltyScreeningForm()} > PrEP Eligibility Screening </Menu.Item>
-                <Menu.Item onClick={() => onClickConsultation()} > PrEP Visit</Menu.Item>
-                <Menu.Item onClick={() => loadPrEPDiscontinuationsInterruptions()} >PrEP Discontinuations & Interruptions</Menu.Item>
+                {patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPRegistrationForm()} >PrEP Enrollment</Menu.Item>)}
+                {patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPCommencementForm()} >PrEP Commencement</Menu.Item>)}
+                {patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPEligibiltyScreeningForm()} > PrEP Eligibility Screening </Menu.Item>)}
+                {patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => onClickConsultation()} > PrEP Visit</Menu.Item>)}
+                {patientObj.HIVResultAtVisit!=="Positive" && (<Menu.Item onClick={() => loadPrEPDiscontinuationsInterruptions()} >PrEP Discontinuations & Interruptions</Menu.Item>)}
                 <Menu.Item onClick={() => loadPatientHistory(patientObj)} >History</Menu.Item>     
                 </>) }                
                 </Menu>
