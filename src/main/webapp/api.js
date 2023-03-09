a8517b734dfe728f8805d7f8250dd6d5bc2c0730
Mathew77 <mathewadegbite@gmail.com>
2023-03-09 07:24:46 +0100

    fixing QA feedback for release

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 // export const url =  'http://localhost:8383/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc4Mjc2NjYwfQ.heTFhThuYQY2-Z5Tp-8_wnADu8YtBWDx1SzEaEbkEyiFdTnMb0xf6f41F3TFis4mS0nnM5uCvA9FpAkvgzYPjA'
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc4MzYxOTc5fQ.qwXXdzn1uJO6y2sBnQnQBezMQbUPtUztrNZJi0NKkktun9gn7_ISauki6CsMl5LYKgi6J80sRBtrY4GMkJo8fg'
