970a24161bb8cb08aed3236897694fce007c726c
vixxjy <ajorvictor48@gmail.com>
2023-08-14 13:43:30 +0100

    Target group added from HTS

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,5 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-// export const url =  'http://localhost:9090/api/v1/';
-// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjgyMTgwMTU4fQ.mDhXvF-P96uEjAvtZkcuUT4cYmXaWda6E1L75EpLXSSaVWXqpfePc7jDjjTUJRhBQjZEwXwQLFgZiFPyeRwLNA'
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+//export const url = "http://localhost:8383/api/v1/";
+//export const token =
+//  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkyMDUxNjM5fQ.O1m3qGwGjjMHWbG5b4CKQ7FWQhd0iNZTEkKEAFESsd_w2kxdnd8lODca_8NLglxcJjWkGM6A4h37bDghie9AXA";
