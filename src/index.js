8f0dca5e7dc978abc6420a3dfad4ec4f9521643d
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2024-09-20 04:31:13 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter, MemoryRouter as Router,} from 'react-router-dom';
-
-import reportWebVitals from "./../src/main/webapp/reportWebVitals";
 import SimpleReactLightbox from "simple-react-lightbox";
 import  ThemeContext  from "./../src/main/webapp/context/ThemeContext"; 
 
--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzI2Nzg4NTY3fQ._Ut2XeErEeKDu455Ajil9S8G5cUvYStr3Tpg8TIktpjMPXgr9Mhc8YGkYqUCpUukglHTgnSTHtAhHzpocYc8rQ"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzI2ODMwMDQwfQ.mRgmsA4M9PgBEH2nnI6htGJI2Qo1tN66j1r6k8jWJrEYRl_8DjNsdXvVuBW-BQ_l8MMbFw1nPRPrZdOo-cRY5A"
     : new URLSearchParams(window.location.search).get("jwt");
