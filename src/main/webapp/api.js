82b4ea78b72e4ece18275165db3500512a924c0a
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2024-09-21 01:32:00 +0100

    Stop tracking api.js

--- src/main/webapp/api.js
+++
@@ -1,7 +0,0 @@
-export const url =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8383/api/v1/"
-    : "/api/v1/";
-export const token =
-  process.env.NODE_ENV === "development"
-? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzI2ODkwODE4fQ.pgjP1Dy1PnaOwCYGJbez2eXzSXZCKMBCHzLZGlK48G7VIgD4fYpyH1drS3yt_IYWc5J-_tMGx4kmzWKb89PDwQ" : new URLSearchParams(window.location.search).get("jwt");
