27596cda171c0403d8883817bb5f8238894cac33
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2024-10-08 10:14:53 +0100

    Update api.js

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,8 @@
 export const url =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8383/api/v1/"
-    : "/api/v1/";
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8383/api/v1/'
+    : '/api/v1/';
 export const token =
-  process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzI4MjU4MjEwfQ.Cl01sNo2_J88UveqK48wn4AF-DLSmt9SfAemk9VfWgFQADN_KT7IgLww1uU5E_Ank95bZI6EIK1N3k84pYjDvA"
-    : new URLSearchParams(window.location.search).get("jwt");
+  process.env.NODE_ENV === 'development'
+    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzI4NDAwODE5fQ.JbfZO2CUBoil__nMHTRyngma1Yzu6cGWJc87Yq7wvsIOV_4kxFtUySaN4_aZEw4W4fkP_RIpHSwkZzuOqOFQFA'
+    : new URLSearchParams(window.location.search).get('jwt');
