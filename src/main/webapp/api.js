685689ce75e8fc8eb1d8664e6837ef23ebfa92d2
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2024-12-23 13:15:58 +0100

    joint checkedIn patients and prep persons

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,7 +4,7 @@ export const url =
     : '/api/v1/';
 export const token =
   process.env.NODE_ENV === 'development'
-    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM0Njk4MDE3fQ.oAhi3gDRbHXMMGqr44IyCEOfLQK6NZZwFOpYV_AN7QE0LRTjraLMdqvPb9ZgF9-pGMSQU54HjlVmcQQpvkiLAA'
+    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM0OTMzNzI5fQ.g3sGTvBd-tkeOXhyXuywyddBjfVwBzf7WtrK4Xyk5GUvOIrpvr4-QSNW5hf6y8gIvyg1tO3sXDumKjcka6Idzw'
     : new URLSearchParams(window.location.search).get('jwt');
 
 export const wsUrl =
