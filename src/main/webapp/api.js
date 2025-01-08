7be9e5845589762c121ec2c707447b886bdbdf88
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-01-08 11:16:40 +0100

    final changes for release

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : '/api/v1/';
 export const token =
   process.env.NODE_ENV === 'development'
-    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM0Njk4MDE3fQ.oAhi3gDRbHXMMGqr44IyCEOfLQK6NZZwFOpYV_AN7QE0LRTjraLMdqvPb9ZgF9-pGMSQU54HjlVmcQQpvkiLAA'
+    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM2MzM5NDMyfQ.ASz6-jlbPhDutd0fFfpmoSepZ6T00EqTxivzZWATAMrMcMlCm5pdNIVtn7sIgl2G_pRDrp51O7jtOrZhfmOxOQ'
     : new URLSearchParams(window.location.search).get('jwt');
