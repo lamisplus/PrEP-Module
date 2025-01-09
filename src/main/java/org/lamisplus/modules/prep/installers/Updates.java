bd334ee6a777a5ff5cb0d36a6852d322d09cfd55
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-01-09 21:39:36 +0100

    added generic duration adjustment function

--- src/main/java/org/lamisplus/modules/prep/installers/Updates.java
+++ src/main/java/org/lamisplus/modules/prep/installers/Updates.java
@@ -6,7 +6,7 @@ import org.springframework.core.annotation.Order;
 
 @Order(2)
 @Installer(name = "schema-installer-update", description = "Updates the required database tables data",
-        version = 33)
+        version = 34)
 public class Updates extends AcrossLiquibaseInstaller {
     public Updates() {
         super("classpath:installers/prep/schema/updates.xml");
