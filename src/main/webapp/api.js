6794ef1cb2d4d82631b0528d32c6a320b8ff0e96
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-01-09 19:51:24 +0100

    added more fixes to prep duration

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : '/api/v1/';
 export const token =
   process.env.NODE_ENV === 'development'
-    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM2NDM3NzM1fQ.tnV9AdGd0c6I7fX714n7wNgYle4UAiQK5gCAL4Q-u5K0ukdKMWgyCYzuH8f66r0rEV5G3EzMHfl0JBQPFokJFw'
+    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM2NDU3Njg0fQ.FL25xqkbFilP2SZPTtkQPNrLkrn2i52Q-x4ybJKwzmsSPFCCKgjqMOFe3sxPZPqScP6dlNNDieenNDCbYKjnQQ'
     : new URLSearchParams(window.location.search).get('jwt');
