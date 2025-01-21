289836952706a24910748564f916e71f911563fe
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-01-21 11:27:44 +0100

    added prev prep status to interruptions

--- src/main/java/org/lamisplus/modules/prep/service/PrepInterruptionService.java
+++ src/main/java/org/lamisplus/modules/prep/service/PrepInterruptionService.java
@@ -3,31 +3,21 @@ package org.lamisplus.modules.prep.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.lamisplus.modules.base.controller.apierror.EntityNotFoundException;
-import org.lamisplus.modules.base.controller.apierror.IllegalTypeException;
-import org.lamisplus.modules.base.controller.apierror.RecordExistException;
-import org.lamisplus.modules.base.domain.dto.PageDTO;
 import org.lamisplus.modules.base.module.ModuleService;
-import org.lamisplus.modules.base.util.PaginationUtil;
-import org.lamisplus.modules.patient.domain.dto.PersonResponseDto;
 import org.lamisplus.modules.patient.domain.entity.Person;
 import org.lamisplus.modules.patient.repository.PersonRepository;
 import org.lamisplus.modules.patient.repository.VisitRepository;
 import org.lamisplus.modules.patient.service.PersonService;
 import org.lamisplus.modules.patient.service.VisitService;
-import org.lamisplus.modules.prep.domain.dto.*;
-import org.lamisplus.modules.prep.domain.entity.*;
+import org.lamisplus.modules.prep.domain.dto.PrepInterruptionDto;
+import org.lamisplus.modules.prep.domain.dto.PrepInterruptionRequestDto;
+import org.lamisplus.modules.prep.domain.entity.PrepInterruption;
 import org.lamisplus.modules.prep.repository.PrepClinicRepository;
 import org.lamisplus.modules.prep.repository.PrepEligibilityRepository;
 import org.lamisplus.modules.prep.repository.PrepEnrollmentRepository;
 import org.lamisplus.modules.prep.repository.PrepInterruptionRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -49,28 +39,26 @@ public class PrepInterruptionService {
     private final VisitRepository visitRepository;
     private final PrepClinicRepository prepClinicRepository;
     private final PatientActivityService patientActivityService;
-
+    private final PrepInterruptionRepository prepInterruptionRepository;
     private ModuleService moduleService;
 
-    private final PrepInterruptionRepository prepInterruptionRepository;
-
     public Person getPerson(Long personId) {
-        return personRepository.findById (personId)
-                .orElseThrow (() -> new EntityNotFoundException(Person.class, "id", String.valueOf (personId)));
+        return personRepository.findById(personId)
+                .orElseThrow(() -> new EntityNotFoundException(Person.class, "id", String.valueOf(personId)));
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         PrepInterruption interruption = prepInterruptionRepository
                 .findByIdAndFacilityIdAndArchived(id, currentUserOrganizationService.getCurrentUserOrganization(), UN_ARCHIVED)
-                .orElseThrow(()-> new EntityNotFoundException(PrepInterruption.class, "id", String.valueOf(id)));
+                .orElseThrow(() -> new EntityNotFoundException(PrepInterruption.class, "id", String.valueOf(id)));
         interruption.setArchived(ARCHIVED);
         prepInterruptionRepository.save(interruption);
     }
 
-    public PrepInterruptionDto update(Long id, PrepInterruptionDto interruptionDto){
+    public PrepInterruptionDto update(Long id, PrepInterruptionDto interruptionDto) {
         PrepInterruption interruption = prepInterruptionRepository
                 .findByIdAndFacilityIdAndArchived(id, currentUserOrganizationService.getCurrentUserOrganization(), UN_ARCHIVED)
-                .orElseThrow(()-> new EntityNotFoundException(PrepInterruption.class, "id", String.valueOf(id)));
+                .orElseThrow(() -> new EntityNotFoundException(PrepInterruption.class, "id", String.valueOf(id)));
         String uuid = interruption.getUuid();
         interruption = interruptionDtoInterruption(interruptionDto, interruption.getPersonUuid());
         interruption.setArchived(UN_ARCHIVED);
@@ -80,42 +68,43 @@ public class PrepInterruptionService {
         return interruptionToInterruptionDto(prepInterruptionRepository.save(interruption));
     }
 
-    public PrepInterruptionDto getInterruptionById(Long id){
+    public PrepInterruptionDto getInterruptionById(Long id) {
         Long userOrgId = currentUserOrganizationService.getCurrentUserOrganization();
         log.info("USER ORG UNIT ID :" + userOrgId);
         Optional<PrepInterruption> interruption = prepInterruptionRepository
                 .findByIdAndFacilityIdAndArchived(id, currentUserOrganizationService.getCurrentUserOrganization(), UN_ARCHIVED);
-                if (!interruption.isPresent()) {
-                    throw new EntityNotFoundException(PrepInterruption.class, "id", String.valueOf(id));
-                }
+        if (!interruption.isPresent()) {
+            throw new EntityNotFoundException(PrepInterruption.class, "id", String.valueOf(id));
+        }
         return interruptionToInterruptionDto(interruption.get());
     }
 
-    public List<PrepInterruptionDto> getInterruptionByPersonId(Long personId){
+    public List<PrepInterruptionDto> getInterruptionByPersonId(Long personId) {
         List<PrepInterruption> interruptions = prepInterruptionRepository
                 .findAllByPersonUuidAndFacilityIdAndArchived(getPerson(personId).getUuid(),
                         currentUserOrganizationService.getCurrentUserOrganization(), UN_ARCHIVED);
 
         return interruptions.stream()
-                .map(interruption-> interruptionToInterruptionDto(interruption))
+                .map(interruption -> interruptionToInterruptionDto(interruption))
                 .collect(Collectors.toList());
     }
 
     public PrepInterruption interruptionRequestDtoInterruption(PrepInterruptionRequestDto interruptionRequestDto, String personUuid) {
-        if ( interruptionRequestDto == null ) {
+        if (interruptionRequestDto == null) {
             return null;
         }
 
         PrepInterruption prepInterruption = new PrepInterruption();
 
-        prepInterruption.setInterruptionType( interruptionRequestDto.getInterruptionType() );
-        prepInterruption.setInterruptionDate( interruptionRequestDto.getInterruptionDate() );
-        prepInterruption.setDateClientDied( interruptionRequestDto.getDateClientDied() );
-        prepInterruption.setCauseOfDeath( interruptionRequestDto.getCauseOfDeath() );
-        prepInterruption.setSourceOfDeathInfo( interruptionRequestDto.getSourceOfDeathInfo() );
-        prepInterruption.setDateClientReferredOut( interruptionRequestDto.getDateClientReferredOut() );
-        prepInterruption.setFacilityReferredTo( interruptionRequestDto.getFacilityReferredTo() );
-        prepInterruption.setInterruptionReason( interruptionRequestDto.getInterruptionReason() );
+        prepInterruption.setInterruptionType(interruptionRequestDto.getInterruptionType());
+        prepInterruption.setPreviousPrepStatus(interruptionRequestDto.getPreviousPrepStatus());
+        prepInterruption.setInterruptionDate(interruptionRequestDto.getInterruptionDate());
+        prepInterruption.setDateClientDied(interruptionRequestDto.getDateClientDied());
+        prepInterruption.setCauseOfDeath(interruptionRequestDto.getCauseOfDeath());
+        prepInterruption.setSourceOfDeathInfo(interruptionRequestDto.getSourceOfDeathInfo());
+        prepInterruption.setDateClientReferredOut(interruptionRequestDto.getDateClientReferredOut());
+        prepInterruption.setFacilityReferredTo(interruptionRequestDto.getFacilityReferredTo());
+        prepInterruption.setInterruptionReason(interruptionRequestDto.getInterruptionReason());
         prepInterruption.setDateSeroConverted(interruptionRequestDto.getDateSeroConverted());
         prepInterruption.setPersonUuid(personUuid);
         prepInterruption.setDateRestartPlacedBackMedication(interruptionRequestDto.getDateRestartPlacedBackMedication());
@@ -129,21 +118,22 @@ public class PrepInterruptionService {
     }
 
     public PrepInterruption interruptionDtoInterruption(PrepInterruptionDto interruptionDto, String personUuid) {
-        if ( interruptionDto == null ) {
+        if (interruptionDto == null) {
             return null;
         }
 
         PrepInterruption prepInterruption = new PrepInterruption();
 
         prepInterruption.setId(interruptionDto.getId());
-        prepInterruption.setInterruptionType( interruptionDto.getInterruptionType() );
-        prepInterruption.setInterruptionDate( interruptionDto.getInterruptionDate() );
-        prepInterruption.setDateClientDied( interruptionDto.getDateClientDied() );
-        prepInterruption.setCauseOfDeath( interruptionDto.getCauseOfDeath() );
-        prepInterruption.setSourceOfDeathInfo( interruptionDto.getSourceOfDeathInfo() );
-        prepInterruption.setDateClientReferredOut( interruptionDto.getDateClientReferredOut() );
-        prepInterruption.setFacilityReferredTo( interruptionDto.getFacilityReferredTo() );
-        prepInterruption.setInterruptionReason( interruptionDto.getInterruptionReason() );
+        prepInterruption.setInterruptionType(interruptionDto.getInterruptionType());
+        prepInterruption.setPreviousPrepStatus(interruptionDto.getPreviousPrepStatus());
+        prepInterruption.setInterruptionDate(interruptionDto.getInterruptionDate());
+        prepInterruption.setDateClientDied(interruptionDto.getDateClientDied());
+        prepInterruption.setCauseOfDeath(interruptionDto.getCauseOfDeath());
+        prepInterruption.setSourceOfDeathInfo(interruptionDto.getSourceOfDeathInfo());
+        prepInterruption.setDateClientReferredOut(interruptionDto.getDateClientReferredOut());
+        prepInterruption.setFacilityReferredTo(interruptionDto.getFacilityReferredTo());
+        prepInterruption.setInterruptionReason(interruptionDto.getInterruptionReason());
         prepInterruption.setDateSeroConverted(interruptionDto.getDateSeroConverted());
         prepInterruption.setPersonUuid(personUuid);
         prepInterruption.setDateRestartPlacedBackMedication(interruptionDto.getDateRestartPlacedBackMedication());
@@ -157,21 +147,22 @@ public class PrepInterruptionService {
 
 
     public PrepInterruptionDto interruptionToInterruptionDto(PrepInterruption prepInterruption) {
-        if ( prepInterruption == null ) {
+        if (prepInterruption == null) {
             return null;
         }
 
         PrepInterruptionDto prepInterruptionDto = new PrepInterruptionDto();
 
         prepInterruptionDto.setId(prepInterruption.getId());
-        prepInterruptionDto.setInterruptionType( prepInterruption.getInterruptionType() );
-        prepInterruptionDto.setInterruptionDate( prepInterruption.getInterruptionDate() );
-        prepInterruptionDto.setDateClientDied( prepInterruption.getDateClientDied() );
-        prepInterruptionDto.setCauseOfDeath( prepInterruption.getCauseOfDeath() );
-        prepInterruptionDto.setSourceOfDeathInfo( prepInterruption.getSourceOfDeathInfo() );
-        prepInterruptionDto.setDateClientReferredOut( prepInterruption.getDateClientReferredOut() );
-        prepInterruptionDto.setFacilityReferredTo( prepInterruption.getFacilityReferredTo() );
-        prepInterruptionDto.setInterruptionReason( prepInterruption.getInterruptionReason() );
+        prepInterruptionDto.setInterruptionType(prepInterruption.getInterruptionType());
+        prepInterruptionDto.setPreviousPrepStatus(prepInterruption.getPreviousPrepStatus());
+        prepInterruptionDto.setInterruptionDate(prepInterruption.getInterruptionDate());
+        prepInterruptionDto.setDateClientDied(prepInterruption.getDateClientDied());
+        prepInterruptionDto.setCauseOfDeath(prepInterruption.getCauseOfDeath());
+        prepInterruptionDto.setSourceOfDeathInfo(prepInterruption.getSourceOfDeathInfo());
+        prepInterruptionDto.setDateClientReferredOut(prepInterruption.getDateClientReferredOut());
+        prepInterruptionDto.setFacilityReferredTo(prepInterruption.getFacilityReferredTo());
+        prepInterruptionDto.setInterruptionReason(prepInterruption.getInterruptionReason());
         prepInterruptionDto.setDateSeroConverted(prepInterruption.getDateSeroConverted());
         prepInterruptionDto.setDateRestartPlacedBackMedication(prepInterruption.getDateRestartPlacedBackMedication());
         prepInterruptionDto.setLinkToArt(prepInterruption.getLinkToArt());
