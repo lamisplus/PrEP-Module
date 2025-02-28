6385abe9cc090bb09f6788d4896a92ff6e68e9d7
gamalieldashuaDataFi <87187530+Gamey001@users.noreply.github.com>
2025-02-28 18:23:53 +0100

    Update api.js

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : '/api/v1/';
 export const token =
   process.env.NODE_ENV === 'development'
-    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzM3MDE2MzYxfQ.el-Fck7qLq9x8BZJSN0TYREQSqTchK3myY-Em6TS4uOOshKx2FvvoMHX8GOTvy8raCeWTR8cqGqBeqwbOOV_iw'
+    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQwNzkwNzY1fQ.9iRwUmkVr8Ioz37p9-zKeorWoEFhDhDxEb-DC69xtnKmH-rAcecU1W4FdXDAnDzOtOf65yG_nUxz6b6vSdfmZg'
     : new URLSearchParams(window.location.search).get('jwt');
