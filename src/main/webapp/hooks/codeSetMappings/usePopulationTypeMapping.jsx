ddfb61b7300bd79e243b32e8afc3eb83aabda876
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-04-17 15:05:58 +0100

    Create usePopulationTypeMapping.jsx

---
+++ src/main/webapp/hooks/codeSetMappings/usePopulationTypeMapping.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function usePopulationTypeMapping() {
+  const populationTypeMapping = {};
+  const getMappedPopulationType = populationType => {};
+  return {
+    getMappedPopulationType,
+  };
+}
+
+export default usePopulationTypeMapping;
