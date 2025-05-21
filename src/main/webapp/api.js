b4147c01a5b66f43de146e31a78bc81a170637b3
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-05-21 12:58:11 +0100

    added the latest build

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,7 +4,7 @@ export const url =
     : '/api/v1/';
 export const token =
   process.env.NODE_ENV === 'development'
-    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ2NDYyMjk1fQ.raXr-Q1VnBNxB8I4z4zo1iUBtfjIgXbGsXkrrVZz_TNULRM7h5Dy3jrMxUjzl_I0dgLpZ1d93hO6Uq7wJeC4sw'
+    ? 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ3ODUwNzE3fQ.vkzAbpAX3mRI2SnL5RLLyZEdcS3npVksUby0OyJ84GBbI8br9dwr6ijROQgVtTOI1641b0HYr2YWexOXn00CQA'
     : new URLSearchParams(window.location.search).get('jwt');
 
 export const wsUrl =
