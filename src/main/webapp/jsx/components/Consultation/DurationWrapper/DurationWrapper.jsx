eb52f139033e073765a0d62d0d50502839fe93eb
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-06-06 11:03:39 +0100

    Create DurationWrapper.jsx

---
+++ src/main/webapp/jsx/components/Consultation/DurationWrapper/DurationWrapper.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import InjectiblesDurationInput from '../InjectiblesDurationInput/InjectiblesDurationInput';
+import OralsDurationInput from '../OralsDurationInput/OralsDurationInput';
+
+const durationInputMap = {
+  PREP_TYPE_INJECTIBLES: props => <InjectiblesDurationInput {...props} />,
+  PREP_TYPE_ORAL: props => <OralsDurationInput {...props} />,
+  DEFAULT: props => <OralsDurationInput {...props} />,
+};
+
+const DurationWrapper = ({
+  prepType,
+  name,
+  value,
+  handleInputChange,
+  style,
+  disabledField,
+  ...restOfProps
+}) => {
+  const inputField = durationInputMap[prepType];
+  console.log('input field: ', inputField, prepType);
+  return inputField
+    ? inputField({
+        name,
+        value,
+        handleInputChange,
+        style,
+        disabledField,
+        ...restOfProps,
+      })
+    : null;
+};
+
+export default DurationWrapper;
