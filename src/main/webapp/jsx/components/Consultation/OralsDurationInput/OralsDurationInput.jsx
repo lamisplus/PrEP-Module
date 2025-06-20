687c329655cf6e8b3ed5d82b577ffb19e52462a6
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-06-20 01:48:18 +0100

    Update OralsDurationInput.jsx

--- src/main/webapp/jsx/components/Consultation/OralsDurationInput/OralsDurationInput.jsx
+++ src/main/webapp/jsx/components/Consultation/OralsDurationInput/OralsDurationInput.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { Label as FormLabelName, Input } from 'reactstrap';
+import { CleanupWrapper } from '../Home';
 const OralsDurationInput = ({
   name,
   value,
   handleInputChange,
   style,
   disabledField,
+  setObjValues,
 }) => {
+  console.log('setObjValues: ', setObjValues);
   return (
-    <Input
-      type={'number'}
-      name={name}
-      id={name}
-      value={value}
-      min={0}
-      onChange={handleInputChange}
-      style={style}
-      disabled={disabledField}
-    />
+    <CleanupWrapper
+      cleanup={() =>
+        setObjValues(prev => ({ ...prev, monthsOfRefill: '', duration: '' }))
+      }
+    >
+      <Input
+        type={'number'}
+        name={name}
+        id={name}
+        value={value}
+        min={0}
+        onChange={handleInputChange}
+        style={style}
+        disabled={disabledField}
+      />
+    </CleanupWrapper>
   );
 };
 
