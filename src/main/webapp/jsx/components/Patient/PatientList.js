a473272910542118b3fdbd6236d7a9886be2ae9f
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2026-04-05 14:06:44 +0100

    fix: commented the disable attr temporarily

--- src/main/webapp/jsx/components/Patient/PatientList.js
+++ src/main/webapp/jsx/components/Patient/PatientList.js
@@ -1,38 +1,38 @@
-import React, { useEffect, useState, useRef } from 'react';
-import axios from 'axios';
-import MaterialTable, { MTableToolbar } from 'material-table';
-import { token as token, url as baseUrl } from './../../../api';
-import { forwardRef } from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import { useHistory } from 'react-router-dom';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-import 'react-toastify/dist/ReactToastify.css';
-import 'react-widgets/dist/css/react-widgets.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import '@reach/menu-button/styles.css';
-import Moment from 'moment';
-import momentLocalizer from 'react-widgets-moment';
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import MaterialTable, { MTableToolbar } from "material-table";
+import { token as token, url as baseUrl } from "./../../../api";
+import { forwardRef } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { useHistory } from "react-router-dom";
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+import "react-toastify/dist/ReactToastify.css";
+import "react-widgets/dist/css/react-widgets.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import "@reach/menu-button/styles.css";
+import Moment from "moment";
+import momentLocalizer from "react-widgets-moment";
 
-Moment.locale('en');
+Moment.locale("en");
 momentLocalizer();
 
 const tableIcons = {
@@ -61,9 +61,9 @@ const tableIcons = {
 
 const useStyles = makeStyles({
   statusLabel: {
-    width: '150px',
-    display: 'inline-block',
-    textAlign: 'center',
+    width: "150px",
+    display: "inline-block",
+    textAlign: "center",
   },
 });
 
@@ -71,7 +71,7 @@ const EnrollPatientButton = ({ row }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const history = useHistory();
 
-  const handleClick = (event) => {
+  const handleClick = event => {
     event.preventDefault();
     event.stopPropagation();
     setAnchorEl(event.currentTarget);
@@ -81,10 +81,10 @@ const EnrollPatientButton = ({ row }) => {
     setAnchorEl(null);
   };
 
-  const handleEnroll = (screeningType) => {
+  const handleEnroll = screeningType => {
     handleClose();
     history.push({
-      pathname: '/patient-dashboard',
+      pathname: "/patient-dashboard",
       state: { patientObj: row, screeningType },
     });
   };
@@ -99,13 +99,13 @@ const EnrollPatientButton = ({ row }) => {
         size="small"
         onClick={handleClick}
         style={{
-          backgroundColor: 'rgb(153, 46, 98)',
-          color: '#fff',
-          height: '30px',
-          width: '215px',
-          fontSize: '12px',
-          fontWeight: 'bolder',
-          textTransform: 'none',
+          backgroundColor: "rgb(153, 46, 98)",
+          color: "#fff",
+          height: "30px",
+          width: "215px",
+          fontSize: "12px",
+          fontWeight: "bolder",
+          textTransform: "none",
         }}
         startIcon={<PersonAddIcon />}
         endIcon={<ArrowDropDownIcon />}
@@ -119,14 +119,14 @@ const EnrollPatientButton = ({ row }) => {
         onClose={handleClose}
       >
         <MenuItem
-          onClick={() => handleEnroll('PrEP')}
-          disabled={!canScreenForPrep}
+          onClick={() => handleEnroll("PrEP")}
+          // disabled={!canScreenForPrep}
         >
           PrEP
         </MenuItem>
         <MenuItem
-          onClick={() => handleEnroll('PEP')}
-          disabled={!canScreenForPep}
+          onClick={() => handleEnroll("PEP")}
+          // disabled={!canScreenForPep}
         >
           PEP
         </MenuItem>
@@ -174,15 +174,15 @@ const Patients = props => {
         icons={tableIcons}
         title="Find Patient"
         columns={[
-          { title: 'Patient Name', field: 'name', hidden: showPPI },
+          { title: "Patient Name", field: "name", hidden: showPPI },
           {
-            title: 'Hospital Number',
-            field: 'hospital_number',
+            title: "Hospital Number",
+            field: "hospital_number",
             filtering: false,
           },
-          { title: 'Sex', field: 'gender', filtering: false },
-          { title: 'Age', field: 'age', filtering: false },
-          { title: 'Actions', field: 'actions', filtering: false },
+          { title: "Sex", field: "gender", filtering: false },
+          { title: "Age", field: "age", filtering: false },
+          { title: "Actions", field: "actions", filtering: false },
         ]}
         data={query =>
           new Promise((resolve, reject) => {
@@ -195,9 +195,9 @@ const Patients = props => {
               .then(result => {
                 resolve({
                   data: result?.data?.records?.map?.(row => ({
-                    name: row.firstName + ' ' + row.surname,
+                    name: row.firstName + " " + row.surname,
                     hospital_number: row.hospitalNumber,
-                    gender: row && row.gender ? row.gender : '',
+                    gender: row && row.gender ? row.gender : "",
                     age: row.age,
                     actions: <EnrollPatientButton row={row} />,
                   })),
@@ -209,15 +209,15 @@ const Patients = props => {
         }
         options={{
           headerStyle: {
-            backgroundColor: '#014d88',
-            color: '#fff',
+            backgroundColor: "#014d88",
+            color: "#fff",
           },
           searchFieldStyle: {
-            width: '100%',
-            margingLeft: '250px',
+            width: "100%",
+            margingLeft: "250px",
           },
           filtering: false,
-          searchFieldAlignment: 'left',
+          searchFieldAlignment: "left",
           pageSizeOptions: [10, 20, 100],
           pageSize: 10,
           debounceInterval: 400,
@@ -235,14 +235,14 @@ const Patients = props => {
                   checked={showPPI === true ? false : true}
                   onChange={handleCheckBox}
                   style={{
-                    border: '1px solid #014D88',
-                    borderRadius: '0.25rem',
+                    border: "1px solid #014D88",
+                    borderRadius: "0.25rem",
                   }}
                 />
                 <label className="form-check-label" htmlFor="basic_checkbox_1">
-                  <b style={{ color: '#014d88', fontWeight: 'bold' }}>
-                    {' '}
-                    SHOW PII{' '}
+                  <b style={{ color: "#014d88", fontWeight: "bold" }}>
+                    {" "}
+                    SHOW PII{" "}
                   </b>
                 </label>
               </div>
