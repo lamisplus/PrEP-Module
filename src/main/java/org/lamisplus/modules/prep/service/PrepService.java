c07b3d1faebc2f064b1a566e2cadc43ced471771
gamalieldashuaDataFi <172114081+gamalieldashuaDataFi@users.noreply.github.com>
2026-06-04 10:36:42 +0100

    fix: fixed lft

--- src/main/java/org/lamisplus/modules/prep/service/PrepService.java
+++ src/main/java/org/lamisplus/modules/prep/service/PrepService.java
@@ -1037,6 +1037,7 @@ public class PrepService {
         prepClinic.setHepatitis(prepClinicRequestDto.getHepatitis());
         prepClinic.setSyphilis(prepClinicRequestDto.getSyphilis());
         prepClinic.setOtherTestsDone(prepClinicRequestDto.getOtherTestsDone());
+        prepClinic.setLiverFunctionTestResults(prepClinicRequestDto.getLiverFunctionTestResults());
         prepClinic.setSyndromicStiScreening(prepClinicRequestDto.getSyndromicStiScreening());
         prepClinic.setRiskReductionServices(prepClinicRequestDto.getRiskReductionServices());
         prepClinic.setDuration(prepClinicRequestDto.getDuration());
@@ -1083,6 +1084,7 @@ public class PrepService {
         prepClinicDto.setHepatitis(clinic.getHepatitis());
         prepClinicDto.setSyphilis(clinic.getSyphilis());
         prepClinicDto.setOtherTestsDone(clinic.getOtherTestsDone());
+        prepClinicDto.setLiverFunctionTestResults(clinic.getLiverFunctionTestResults());
         prepClinicDto.setSyndromicStiScreening(clinic.getSyndromicStiScreening());
         prepClinicDto.setRiskReductionServices(clinic.getRiskReductionServices());
         prepClinicDto.setDuration(clinic.getDuration());
