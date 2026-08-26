696f72ca0002160e7f98bc0c3b93d2e96b3695e4
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2026-08-26 16:22:42 +0100

    fix: fixed prep hts_encounter_uuid

--- src/main/java/org/lamisplus/modules/prep/installers/Updates.java
+++ src/main/java/org/lamisplus/modules/prep/installers/Updates.java
@@ -7,7 +7,7 @@ import org.springframework.core.annotation.Order;
 @Order(4)
 
 @Installer(name = "schema-installer-update", description = "Updates the required database tables data",
-        version = 96)
+        version = 103)
 public class Updates extends AcrossLiquibaseInstaller {
     public Updates() {
         super("classpath:installers/prep/schema/updates.xml");
